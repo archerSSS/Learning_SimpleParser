4b7228a69573d76b53bd3cd313eb26f54a568f19
archyrysss <archyrysss@gmail.com>
2021-03-07 23:00:39 +0300

    SimpleParser_v.0.11

---
+++ SimpleParser/AgileParser.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleParser
+{
+    class AgileParser
+    {
+        public void Parse()
+        {
+            Dictionary<string, List<List<string>>> dict = Parse("https://www.readfootball.com/tables.html");
+            Dictionary<string, List<List<string>>> dict2 = Parse_Beta("https://www.google.com/");
+        }
+
+        public Dictionary<string, List<List<string>>> Parse(string url)
+        {
+            try
+            {
+                Dictionary<string, List<List<string>>> result = new Dictionary<string, List<List<string>>>();
+
+                using (HttpClientHandler handler = new HttpClientHandler() { AllowAutoRedirect = false, AutomaticDecompression = System.Net.DecompressionMethods.Deflate | System.Net.DecompressionMethods.GZip | System.Net.DecompressionMethods.None })
+                {
+                    using (var client = new HttpClient(handler))
+                    {
+                        using (HttpResponseMessage response = client.GetAsync(url).Result)
+                        {
+                            if (response.IsSuccessStatusCode)
+                            {
+                                var html = response.Content.ReadAsStringAsync().Result;
+                                if (!string.IsNullOrEmpty(html))
+                                {
+                                    HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+                                    doc.LoadHtml(html);
+                                    
+                                    var tables = doc.DocumentNode.SelectNodes(".//div[@class='block_content']//div[@class='two-table-row']//div[@class]");
+                                    if (tables != null && tables.Count > 0)
+                                    {
+                                        foreach (var table in tables)
+                                        {
+                                            var titleNode = table.SelectSingleNode(".//div[@class='head_tb']");
+                                            if (titleNode != null)
+                                            {
+                                                var table_b = table.SelectSingleNode(".//div[@class='tab_champ']//table");
+                                                if (table_b != null)
+                                                {
+                                                    var rows = table_b.SelectNodes(".//tr");
+                                                    if (rows != null && rows.Count > 0)
+                                                    {
+                                                        var res = new List<List<string>>();
+
+                                                        foreach (var row in rows) 
+                                                        {
+                                                            var cells = row.SelectNodes(".//td");
+                                                            if (cells != null && cells.Count > 0)
+                                                            {
+                                                                res.Add(new List<string>(cells.Select(c => c.InnerText)));
+                                                            }
+                                                        }
+
+                                                        result[titleNode.InnerText] = res;
+                                                    }
+                                                }
+                                            }
+                                        }
+
+                                        return result;
+                                    }
+                                    else { Console.WriteLine("Oooooopssss"); }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+
+            return null;
+        }
+
+        public Dictionary<string, List<List<string>>> Parse_Beta(string url)
+        {
+            try
+            {
+                Dictionary<string, List<List<string>>> result = new Dictionary<string, List<List<string>>>();
+
+                using (HttpClientHandler handler = new HttpClientHandler() { AllowAutoRedirect = false, AutomaticDecompression = System.Net.DecompressionMethods.Deflate | System.Net.DecompressionMethods.GZip | System.Net.DecompressionMethods.None })
+                {
+                    using (var client = new HttpClient(handler))
+                    {
+                        using (HttpResponseMessage response = client.GetAsync(url).Result)
+                        {
+                            if (response.IsSuccessStatusCode)//
+                            {
+                                var html = response.Content.ReadAsStringAsync().Result;
+                                if (!string.IsNullOrEmpty(html))
+                                {
+                                    HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+                                    doc.LoadHtml(html);
+
+                                    //var el = doc.GetElementbyId("skip-link");
+                                    var tab = doc.DocumentNode.SelectNodes(".//div[@class]");
+                                    foreach (HtmlAgilityPack.HtmlNode node in tab)
+                                    {
+                                        string s = "";
+                                        foreach (HtmlAgilityPack.HtmlAttribute att in node.Attributes)
+                                        {
+                                            if (att.Name == "class") Console.WriteLine("Class: " + att.Value); 
+                                            
+                                        }
+                                        //HtmlAgilityPack.HtmlNode n = GetPointedNode("o3j99 n1xJcf Ne6nSd", node);
+                                    }
+
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+
+            return null;
+        }
+
+        private HtmlAgilityPack.HtmlNode GetPointedNode(string className, HtmlAgilityPack.HtmlNode node)
+        {
+            if (node.ChildNodes.Count == 0) return null;
+            foreach (HtmlAgilityPack.HtmlNode htmlNode in node.ChildNodes)
+            {
+                string name = htmlNode.Name;
+                if (htmlNode.Name == className)
+                {
+                    return htmlNode;
+                }
+
+                HtmlAgilityPack.HtmlNode nextNode = GetPointedNode(className, htmlNode);
+                if (nextNode != null)
+                {
+                    return nextNode;
+                }
+            }
+
+            return null;
+        }
+    }
+}
--- SimpleParser/Program.cs
+++ SimpleParser/Program.cs
@@ -1,36 +1,53 @@
 ﻿using System;
 using System.Diagnostics;   // Нужна, чтобы запускать внешние процессы
+using System.IO;
 using System.Net;           // Нужна, чтобы работать с Web
 using System.Threading;
 
 namespace SimpleParser
 {
+    //https://vk.com/id20210102?z=photo20210102_457243035%2Falbum20210102_0%2Frev
+    //aimp.ru/index.php?do=download&sub=catalog&id=
+    //http://www.aimp.ru/index.php?do=catalog&rec_id=
+
     class Program
     {
         static void Main(string[] args)
         {
+            AgileParser AP = new AgileParser();
+            AP.Parse();
+
+            /*ParserObject PO = new ParserObject();
+            PO.Parsing();*/
+
+            /*string sn = "https://vk.com/id20210102?z=photo20210102_457243035%2Falbum20210102_0%2Frev";
             WebClient wc = new WebClient();
-            Console.WriteLine(DownloadSkinsForAimp(wc));
+            wc.DownloadFile("https://vk.com/id20210102?z=photo20210102_457243035%2Falbum20210102_0%2Frev", "JNX8dh4MuL8.jpg");*/
+            
+
+            //Console.WriteLine(DownloadSkinsForAimp(wc, sn));
         }
 
-        static string DownloadSkinsForAimp(WebClient wc)
+        static string DownloadSkinsForAimp(WebClient wc, string leftPart)
         {
+            string downLoadLeftPart = "";
+
             Console.WriteLine("Downloading began");
             try
             {
-                for (int i = 0; i <= 5; i++)
+                for (int i = 0; i <= 1; i++)
                 {
                     string id = "79" + i;
-                    string name = GetNameOfSkin(wc, id);
+                    string name = "";//GetNameOfSkin(wc, id, downLoadLeftPart);
 
                     // Путь, откуда мы будем скачивать
-                    string path = "aimp.ru/index.php?do=download&sub=catalog&id=" + id;
+                    string path = leftPart + id;
 
                     try
                     {
                         // Запускаем нужный нам браузер и передаем ему в качестве аргумента путь скачивания
                         // google chrome
-                        Process.Start("chrome.exe", path);
+                        Process.Start("chrome.exe", "https://vk.com/id20210102?z=photo20210102_457243035%2Falbum20210102_0%2Frev");
 
                         Console.WriteLine("Download " + name + " is succesfull!");
 
@@ -51,10 +68,11 @@ namespace SimpleParser
 
         }
 
-        static string GetNameOfSkin(WebClient wc, string id)
+        static string GetNameOfSkin(WebClient wc, string id, string leftPart)
         {
             // Получаем строку с html разметкой
-            string html = wc.DownloadString("http://www.aimp.ru/index.php?do=catalog&rec_id=" + id);
+            string html = wc.DownloadString(leftPart + id);
+            
             // Находим в ней первое упоминание нужного нам id и удаляем ненужную левую часть
             // Название скина начинается через 5 символов после этого id
             string rightPartOfHtml = html.Substring(html.IndexOf(id) + 5);
